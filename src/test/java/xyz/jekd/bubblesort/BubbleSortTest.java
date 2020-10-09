d6d41303c81fbebbcd5f7463b4262658591b81e7
JinRu Xian <jimru.xian@tenfoldtech.com>
2020-10-09 22:22:24 +0800

    BS: 改进单元测试 See

--- src/test/java/xyz/jekd/bubblesort/BubbleSortTest.java
+++ src/test/java/xyz/jekd/bubblesort/BubbleSortTest.java
@@ -21,12 +21,30 @@ public class BubbleSortTest {
 
     @Test
     public void test_sort__shuffled_Integer() {
-        Integer[] arr = {1, 24, 3, 67, 5};
-        Integer[] expect = {1, 3 ,5, 24, 67};
         BubbleSort bs = new BubbleSort();
-        Integer[] sortedArr = bs.sort(arr);
 
-        assertArrayEquals(expect, sortedArr);
+        Integer[] expect = {1, 3, 5};
+
+        Integer[] arr1 = {1, 3, 5};
+        Integer[] arr2 = {1, 5, 3};
+        Integer[] arr3 = {3, 1, 5};
+        Integer[] arr4 = {3, 5, 1};
+        Integer[] arr5 = {5, 1, 3};
+        Integer[] arr6 = {5, 3, 1};
+
+        Integer[] sortedArr1 = bs.sort(arr1);
+        Integer[] sortedArr2 = bs.sort(arr2);
+        Integer[] sortedArr3 = bs.sort(arr3);
+        Integer[] sortedArr4 = bs.sort(arr4);
+        Integer[] sortedArr5 = bs.sort(arr5);
+        Integer[] sortedArr6 = bs.sort(arr6);
+
+        assertArrayEquals(expect, sortedArr1);
+        assertArrayEquals(expect, sortedArr2);
+        assertArrayEquals(expect, sortedArr3);
+        assertArrayEquals(expect, sortedArr4);
+        assertArrayEquals(expect, sortedArr5);
+        assertArrayEquals(expect, sortedArr6);
     }
 
     // -----------------------------------
